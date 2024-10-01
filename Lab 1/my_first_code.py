f485fe636f05a6d819dab0a874b3ab42bfbfbdbd
Bardan Gurung (s5731853) <s5731853@bournemouth.ac.uk>
2024-10-01 10:33:32 +0100

    changed the print text

---
+++ Lab 1/my_first_code.py
@@ -0,0 +1,39 @@
+import math
+
+print("My first Python code")
+
+print("1 + 2") #string
+
+print(1 + 2) #number
+
+print("Hello", end=" ")
+print("dead")
+
+
+name = "Bardan"
+age = 18
+
+print("hello" + name)
+print(f"hello my name is {name} and I am {age} years old")
+
+print(265 + 5924)
+print(5022 - 901)
+print(5 * 10)
+print(604/4)
+print(3 % 2)
+print(10 % 4)
+print(9 // 3)
+print(8 // 3)
+print(5 ** 2)
+print(4 ** 3)
+
+a = 265
+b = 5924
+result1 = a + b
+print(f"a + b = {result1}")
+
+c = 6 * 4
+d = 5 ** 2
+e = 4 % 3
+result2 = c + d - e
+print(result2)
